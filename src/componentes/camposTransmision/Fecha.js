308138d2b3bc3b32be8ca826c0b01ce31b505600
Abad Carrascosa <Alejandro_AC@hefame.es>
2020-01-16 17:13:15 +0100

    Cambiado formato y habilitado modo compacto

--- src/componentes/camposTransmision/Fecha.js
+++ src/componentes/camposTransmision/Fecha.js
@@ -8,16 +8,38 @@ const dateToHuman = (date) => {
     return date.getDate().toString().padStart(2, '0') + '/' +
         (date.getMonth() + 1).toString().padStart(2, '0') + '/' +
         date.getFullYear() + ' ' +
-        date.toLocaleTimeString('es-ES');
+        date.toLocaleTimeString('es-ES').padStart(8, '0');
 
 }
 
 
+const dateToHumanCorto = (date) => {
+    date = new Date(date);
+
+    return date.getDate().toString().padStart(2, '0') + '/' +
+        (date.getMonth() + 1).toString().padStart(2, '0') + '/' +
+        (date.getFullYear() % 1000) + ' ' +
+        date.toLocaleTimeString('es-ES').padStart(8, '0');
+
+}
+
 const Fecha = (props) => {
 
-    let cadena = dateToHuman(props.fecha);
+    let formato = props.formato || 'normal'
+    let cadena = '';
+    let icono = null;
 
-    return <span><MdAccessTime size={18} className="text-info" style={{paddingBottom: '3px'}} />{cadena}</span>;
+    if (formato === 'corto') {
+        cadena = dateToHumanCorto(props.fecha);
+    } else {
+        cadena = dateToHuman(props.fecha);
+        icono = <MdAccessTime size={20} className="text-info mr-1" style={{ paddingBottom: '1px' }} />
+    }
+
+    return <code className="text-reset">
+        {icono}{cadena}
+    </code>;
+
 
 
 }
