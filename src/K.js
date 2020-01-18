932b0349f8fe537c5fee945e8fc1cb7b329de337
Alejandro AC <alejandro.abad@hefame.es>
2020-01-18 13:02:51 +0100

    Actualizadas direcciones IP

--- src/K.js
+++ src/K.js
@@ -2,7 +2,7 @@ export default {
     PRODUCCION: false,
     DESTINOS: {
         CORE: 'https://fedicom3-dev.hefame.es',
-        MONITOR: 'https://cpd25.hefame.es:8443'
+        MONITOR: 'https://f3dev.hefame.es:8443'
     },
     AVISO_JWT_PROXIMO_A_CADUCAR: 60*5
 };
